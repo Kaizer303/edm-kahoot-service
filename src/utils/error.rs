791c0e7a6faf6a55480e5dd941e2c831c28cc282
narongchai <narongchai@thinknet.co.th>
2024-10-31 13:51:34 +0700

    - update AppError

--- src/utils/error.rs
+++ src/utils/error.rs
@@ -1,6 +1,23 @@
+use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct AppError {
+    pub status: u16,
     pub error: String,
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, Json(self)).into_response()
+    }
+}
+
+impl AppError {
+    pub fn new(status: StatusCode, error: String) -> Self {
+        Self {
+            status: status.as_u16(),
+            error,
+        }
+    }
 }
